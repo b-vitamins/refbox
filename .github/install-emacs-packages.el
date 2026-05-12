37f78ddd84bef413ebe41648d6cce605303866a3
Ayan Das <bvits@riseup.net>
2026-05-12 09:22:39 +0530

    fix(ci): upgrade bundled Emacs packages

--- .github/install-emacs-packages.el
+++ .github/install-emacs-packages.el
@@ -4,14 +4,18 @@
 
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")))
+(setq package-install-upgrade-built-in t)
 
 (package-initialize)
 (package-refresh-contents)
 
 (defun refbox-ci-install-package (package minimum-version)
   "Install PACKAGE unless at least MINIMUM-VERSION is available."
-  (unless (package-installed-p package (version-to-list minimum-version))
-    (package-install package)))
+  (let ((minimum (version-to-list minimum-version)))
+    (unless (package-installed-p package minimum)
+      (package-install package))
+    (unless (package-installed-p package minimum)
+      (error "%s %s or newer is required" package minimum-version))))
 
 (refbox-ci-install-package 'org "9.8")
 
